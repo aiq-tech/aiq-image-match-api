a003e109c63114ee80c7a02a98ae2ba50809924c
Kane O'Riley <koriley@nextfaze.com>
2015-10-01 09:42:22 +0930

    Point to cloud-capi

--- example-iqkit-core/src/main/java/org/iqnect/example/iqkitcore/Application.java
+++ example-iqkit-core/src/main/java/org/iqnect/example/iqkitcore/Application.java
@@ -1,6 +1,10 @@
 package org.iqnect.example.iqkitcore;
 
+import android.net.Uri;
+import android.text.TextUtils;
+
 import org.iqnect.iqkit.IQKit;
+import org.iqnect.iqkit.IQKitConfiguration;
 
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -17,7 +21,23 @@ public class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
 
-        mIQKit = IQKit.init(this, BuildConfig.IQNECT_APP_ID, BuildConfig.IQNECT_APP_SECRET);
+        // TODO: remove when all servers are setup
+        String serverUrl = TextUtils.isEmpty(BuildConfig.IQNECT_APP_SERVER) ?
+                "http://cloud-capi.iqnect.org" : BuildConfig.IQNECT_APP_SERVER;
+
+        if (TextUtils.isEmpty(serverUrl)) {
+            log.debug("using default server");
+            mIQKit = IQKit.init(this, BuildConfig.IQNECT_APP_ID, BuildConfig.IQNECT_APP_SECRET);
+        } else {
+            log.debug("using custom server: {}", serverUrl);
+            IQKitConfiguration config = new IQKitConfiguration(this);
+
+            config.setAppId(BuildConfig.IQNECT_APP_ID);
+            config.setAppSecret(BuildConfig.IQNECT_APP_SECRET);
+            config.setBaseUri(Uri.parse(serverUrl));
+
+            mIQKit = IQKit.init(config);
+        }
         log.debug("instantiated iqkit: {}", mIQKit);
     }
 
--- example-iqkit-ui/src/main/java/org/iqnect/example/iqkitui/Application.java
+++ example-iqkit-ui/src/main/java/org/iqnect/example/iqkitui/Application.java
@@ -1,10 +1,13 @@
 package org.iqnect.example.iqkitui;
 
+import android.net.Uri;
 import android.support.multidex.MultiDexApplication;
+import android.text.TextUtils;
 
 import com.nextfaze.logging.Logging;
 
 import org.iqnect.iqkit.IQKit;
+import org.iqnect.iqkit.IQKitConfiguration;
 
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -24,6 +27,23 @@ public class Application extends MultiDexApplication {
         Logging.configure(this, "log4j.properties");
         log.debug("application started");
 
-        mIQKit = IQKit.init(this, BuildConfig.IQNECT_APP_ID, BuildConfig.IQNECT_APP_SECRET);
+        // TODO: remove when all servers are setup
+        String serverUrl = TextUtils.isEmpty(BuildConfig.IQNECT_APP_SERVER) ?
+                "http://cloud-capi.iqnect.org" : BuildConfig.IQNECT_APP_SERVER;
+
+        if (TextUtils.isEmpty(serverUrl)) {
+            log.debug("using default server");
+            mIQKit = IQKit.init(this, BuildConfig.IQNECT_APP_ID, BuildConfig.IQNECT_APP_SECRET);
+        } else {
+            log.debug("using custom server: {}", serverUrl);
+            IQKitConfiguration config = new IQKitConfiguration(this);
+
+            config.setAppId(BuildConfig.IQNECT_APP_ID);
+            config.setAppSecret(BuildConfig.IQNECT_APP_SECRET);
+            config.setBaseUri(Uri.parse(serverUrl));
+
+            mIQKit = IQKit.init(config);
+        }
+        log.debug("instantiated iqkit: {}", mIQKit);
     }
 }
